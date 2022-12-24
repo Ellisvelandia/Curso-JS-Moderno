a6530d94ed248d53c8c754d8ff32d068d4f33bd9
Ellisvelandia <eyis619l@gmail.com>
2022-12-23 20:47:35 -0500

    js

--- 01-Introduccion/js/script.js
+++ 01-Introduccion/js/script.js
@@ -1,22 +1,32 @@
-// alert("hello world")
+// // // alert("hello world")
 
-// const name = prompt("What is your name")
+// // // const name = prompt("What is your name")
 
-//take the name and show on window!
-// document.querySelector(".contenido").innerHTML = `${name} is learning modern javascript`
+// // //take the name and show on window!
+// // // document.querySelector(".contenido").innerHTML = `${name} is learning modern javascript`
 
-//assign the value to the product variable
-// const product = '24 inch monitor';
+// // //assign the value to the product variable
+// // // const product = '24 inch monitor';
 
 
-// console.time("Hello")
-// console.warn("this is not allowed")
+// // // console.time("Hello")
+// // // console.warn("this is not allowed")
 
-console.log('Hello');
-console.log('World');
+// // console.log('Hello');
+// // console.log('World');
 
-function Hello() {
-  console.log('Ok');
-}
+// // function Hello() {
+// //   console.log('Ok');
+// // }
 
-Hello();
+// // Hello();
+
+// let day = 0;
+// let foods = "";
+
+// while (day < 4) {
+//   console.log("eating")
+//   day = day + 1
+// }
+
+// console.log(day , foods)
